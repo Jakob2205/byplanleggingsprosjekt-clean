1213e49f16f284ee57883ca1518bc163a3352b3c
jakobgra <jakobgra@stud.ntnu.no>
2025-02-21 20:30:30 +0100

    Styrer litt med git versjoner

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar = () => {
+    return (
+        <aside className="sidebar">
+            <div className="sidebar-top">
+                <button className="sidebar-button">+ Ny plan</button>
+            </div>
+
+            <div className="sidebar-bottom">
+                Arkiv
+            </div>
+        </aside>
+    );
+};
+
+export default Sidebar;
