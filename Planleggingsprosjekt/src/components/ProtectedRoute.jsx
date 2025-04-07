b4db03977134356c681e2495bb7d9c69520f2862
Jakob <jakobgrande@hotmail.com>
2025-04-07 16:07:02 +0200

    Flattened Planleggingsprosjekt folder into normal project structure

---
+++ Planleggingsprosjekt/src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+// src/components/ProtectedRoute.jsx
+import React from 'react';
+import { useAuth } from '../context/AuthContext';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+    const { user } = useAuth();
+
+    if (!user) {
+        return <Navigate to="/" />;
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
