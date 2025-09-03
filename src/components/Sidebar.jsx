d955df8ecc76f7b04d8e1f3fe260acedf9861a2a
jakobgra <jakobgra@stud.ntnu.no>
2025-09-03 12:52:52 +0200

    Startet opp for å se om github pages kjører

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import FormSelector from "./FormSelector"; // Adjust the path as needed
+
+const Sidebar = ({ selectedForm, onSelectForm }) => {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-top">
+        {/* Render the FormSelector here */}
+        <FormSelector selectedForm={selectedForm} onSelectForm={onSelectForm} />
+      </div>
+      <div className="sidebar-bottom">
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
