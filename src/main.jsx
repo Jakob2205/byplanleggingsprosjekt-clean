1797400f1a2062cfef4e5273b9a21bb0eb192e75
jakobgra <jakobgra@stud.ntnu.no>
2025-09-26 13:24:03 +0200

    Prøver å fikse routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// This code is for handling the GitHub Pages SPA refresh issue.
-// It checks if a redirect path is stored in sessionStorage, and if so,
-// it navigates to that path and then clears the stored path.
-const path = sessionStorage.getItem('redirect');
-if (path) {
-  sessionStorage.removeItem('redirect');
-  // Use replaceState to change the URL without adding to history,
-  // and without triggering a page reload.
-  const newUrl = new URL(window.location.href);
-  window.history.replaceState(null, null, newUrl.origin + path);
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
