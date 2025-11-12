f5978e2166b131fe415e997a6cafc3f9b10a6e56
jakobgra <jakobgra@stud.ntnu.no>
2025-11-12 20:41:20 +0100

    prøver å fikse servertilgang til pages

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 // vite.config.js
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load env variables for the current mode
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  // Create a new object that only contains the VITE_ variables
-  const viteEnv = {};
-  Object.keys(env).forEach((key) => {
-    if (key.startsWith('VITE_')) {
-      viteEnv[`import.meta.env.${key}`] = JSON.stringify(env[key]);
-    }
-  });
-
-  return {
-    plugins: [react()],
-    // Make only the VITE_ variables available in the client-side code
-    define: viteEnv,
-    base: "/byplanleggingsprosjekt-clean/",
-  }
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/byplanleggingsprosjekt-clean/',
+  plugins: [react()],
 })
