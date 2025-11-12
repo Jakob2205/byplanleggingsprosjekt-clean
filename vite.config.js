d4d51763bde02db01e26b60a04bb7c16fb98ba56
jakobgra <jakobgra@stud.ntnu.no>
2025-11-12 21:17:28 +0100

    Forsøk 56666

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// Triggering a new deployment to fetch updated secrets.
 export default defineConfig({
   base: '/byplanleggingsprosjekt-clean/',
   plugins: [react()],
